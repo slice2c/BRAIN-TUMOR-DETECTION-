a29380618cd3f758f95d83a7b6c059b5f655e894
Sid <siddiqmoideen07@gmail.com>
2024-04-03 13:08:35 +0530

    Updated MRInCT Split codeBase

--- codeBase/dataStuff/CTMRIstuff/minData/dataPrep.py
+++ codeBase/dataStuff/CTMRIstuff/minData/dataPrep.py
@@ -9,20 +9,23 @@ import os
 
 t1 = time.time()
 print("Select the Directory Yo!")
-imgDir = filedialog.askdirectory() + '//'
+#imgDir = filedialog.askdirectory()
+imgDir = '/Volumes/ReserveDisk/OpenSourceContribution/SynthCT/codeBase/dataStuff/CTMRIstuff/minData/mergeCTMRI'
+if "\\" in imgDir:
+    imgDir = imgDir + '\\'
+else:
+    imgDir = imgDir + '/'
 print("---->", imgDir)
-cDir = os.getcwd()
-os.chdir(imgDir)
 
-imgList = glob.glob("*.png")
-os.chdir(cDir)
+imgList = glob.glob(imgDir + "*.png")
 for i in tqdm.tqdm(imgList, colour="red"):
     img = cv2.imread(i)
+    i = i.replace(imgDir, '')
     if not os.path.isdir("CTstuff"):
         os.makedirs("CTstuff")
-    imgCT = img[:, :255]
-    cv2.imwrite('CTstuff//'+i, imgCT)
     if not os.path.isdir("MRIstuff"):
         os.makedirs("MRIstuff")
+    imgCT = img[:, :256]
+    cv2.imwrite('CTstuff//'+i, imgCT)
     imgMRI = img[:, 256:]
-    cv2.imwrite('MRIstuff//'+i, imgCT)
+    cv2.imwrite('MRIstuff//'+i, imgMRI)
